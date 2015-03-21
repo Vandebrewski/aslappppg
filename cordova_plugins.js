483ad7f6d193c6da81c23364257a8e599285ed4f
Vandebrewski <Bastiaan@MacBook-Pro-van-Bastiaan.local>
2015-03-21 20:31:28 +0100

    up

---
+++ cordova_plugins.js
@@ -0,0 +1,17 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cc.fovea.cordova.purchase/www/store-ios.js",
+        "id": "cc.fovea.cordova.purchase.InAppPurchase",
+        "clobbers": [
+            "store"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cc.fovea.cordova.purchase": "3.10.1"
+}
+// BOTTOM OF METADATA
+});
